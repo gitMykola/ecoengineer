b2f819d74fbdcb5b333a7de20c3694929ecd9c2c
gitMykola <mykola_borodyn@ecoengineer.in.ua>
2019-03-12 22:03:02 +0200

    TODO list

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,21 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+/*
+  TODO
+  - saving animation add fromTop
+  - footer bottom menu set li color to silver
+  - footer bottom menu set icons padding
+  - footer bottom menu set @media-screen responsive on/off bottom menu
+  - footer add socials
+  - add ankers to start slide buttons
+  - make Home component objects gallery
+  - make Home component contacts frame
+  - add goToTop component
+  - redesign Business Component background to site style
+  - add AskButton Component
+  - design & add Technologies Component
+  - design & add Services Component
+  - REST API & backend...
+*/
